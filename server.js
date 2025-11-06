3baed2dbde0c99f6f85dd36bc34dd9cfa1afada5
OMGBA FOUDA <omgba.joseph@institutsaintjean.org>
2025-11-06 18:36:37 +0100

    yoo

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.static("."));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
